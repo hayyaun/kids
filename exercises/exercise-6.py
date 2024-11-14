9d257c7c6713f21dc59c082243923a990e270d97
hayyaun <hayyanhami@outlook.com>
2024-11-14 13:27:56 +0330

    .

---
+++ exercises/exercise-6.py
@@ -0,0 +1,6 @@
+# Create number guessing game
+#
+# 1. Generate a random number (between 0 and 100) and store it in a variable as integer
+# 2. Loop until user finds the correct number
+# 3. [optional] You can also limit the number of times user is allowed to guess!
+# 4. [optional] You can show the number of times spent to guess the correct number!
---
+++ exercises/exercise-7.py
@@ -0,0 +1,6 @@
+# GUI Calculator
+#
+# Using `tkinter` library, create a calculator app with Graphical User Interface (GUI)
+# 1. There are 2 inputs, each get a number
+# 2. There is a `=` button to calculate `summation`, `subtraction`, `multiplication`, and `divison` of that 2 number.
+# 3. [Optional] You can instead add `+`, `-`, `x`, `/` buttons.
