9612f05bffdcb3004d6fdc114799c5a96112b332
hayyaun <hayyanhami@outlook.com>
2024-12-04 23:05:55 +0330

    .

--- exercises/exercise-3-2.py
+++ exercises/exercise-3-2.py
@@ -2,16 +2,19 @@
 # 1. Get a number from user in Terminal
 # 2. Print each primary number below n
 
-# Complete this code:
+"""  Fix this code  """
 
-# TODO get the number n from user
+# get the number n from user
 
-for i in range(2, n):
-    is_primary = True  # flag
-    
-    # TODO for j in range(2, i):
-        # TODO if i % j == 0:
-            # TODO set is primary False
-            # TODO stop the loop
+# for i in range 2 to n:
 
-    # TODO print number if primary
+#       is_primary = True  # flag
+
+#       for j in range 2 to i:
+
+#           if i % j == 0:
+
+#               set is primary False
+#               stop the loop
+
+#       print number if primary
