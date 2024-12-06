f75bc43fd958352dd8fbb31228391fe865cc2bb8
hayyaun <hayyanhami@outlook.com>
2024-12-06 04:03:17 +0330

    .

--- exercises/exercise-3-3.py
+++
@@ -1,5 +0,0 @@
-# Show Primary numbers below n
-# 1. Get a number from user in Terminal
-# 2. Print each primary number below n
-
-# Help: ./exericse-3-3.md
---
+++ exercises/exercise-4-1.py
@@ -0,0 +1,3 @@
+# Show Prime numbers below n
+# 1. Get a number from user in Terminal
+# 2. Print each prime number below n
