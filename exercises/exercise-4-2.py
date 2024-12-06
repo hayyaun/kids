526a6096630e7e73f36ee347b0137ed4ba7aa107
hayyaun <hayyanhami@outlook.com>
2024-12-06 06:35:26 +0330

    .

---
+++ exercises/exercise-4-2.py
@@ -0,0 +1,15 @@
+# Swap function
+
+
+def swap(a, b):
+
+    print(f'before:\t a = {a}, b = {b}')
+
+    # TODO ...
+
+    print(f'after:\t a = {a}, b = {b}')
+
+
+swap(3, 4)
+swap(8, 5)
+swap(2, 6)
