cd2c8a341d8cb18853a7edbb2806e1afaa35757f
hayyaun <hayyanhami@outlook.com>
2024-12-15 10:35:32 +0330

    .

---
+++ exercises/exercise-5-1.py
@@ -0,0 +1,8 @@
+# Use List to store a Fibbonacci series
+
+n = 10
+fib = []
+
+# TODO fill the fib list to contain 10 items
+
+print(f'Fibbonacci serie: {fib}')
---
+++ exercises/exercise-5-2.py
@@ -0,0 +1,6 @@
+# Fa-En Dictionary
+
+# 1. Create a Fa-En dictionary
+# 2. In a loop: get a word from user
+# 2.1. If available: Show translation
+# 2.2. If not available: Say 404!
