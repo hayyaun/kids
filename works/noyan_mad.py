a6db93b4ac5c970d63ac7fed5119498af00fb341
hayyaun <hayyanhami@outlook.com>
2024-12-18 19:49:08 +0330

    .

---
+++ works/noyan_mad.py
@@ -0,0 +1,26 @@
+print("salam.chi kar dari?! boro gomsho nemikhamet")
+print("aha! pas to baraye ganj oomadi na?khoobe!")
+print("baraye residan be raz makhfi gavsandogh noyan bayad adad ramz ro hads bezani! shayad ham ramz ye jayi bashe.nemidoonam!")
+n = int(input("ramz ro vared kon,4 raghame! "))
+if n == 2525:
+    print("good work!")
+    print("go to telegram and write this link in your saved massage and tap it: https://t.me/+AHLF8Z0nYN0xYmVk ")
+elif n != 2525:
+    print("ok.bezar rahnamayi konam!: if ramz == hayyun's ....: ramz == true")
+n = int(input("lets try again! "))
+if n == 2525:
+    print("good work!")
+    print("go to telegram and write this link in your saved massage and tap it: https://t.me/+AHLF8Z0nYN0xYmVk ")
+elif n != 2525:
+    print("ok baba! yeki dige: its 5*5,5*x if you cant do this,i hack you.im sure.")
+n = int(input("again!!! "))
+if n == 2525:
+    print("good work!")
+    print("go to telegram and write this link in your saved massage and tap it: https://t.me/+AHLF8Z0nYN0xYmVk ")
+elif n != 2525:
+    print("☠️")
+    s = 'you hacked ip.piiokoko'
+    i = 1
+    while True:
+        print(s, i)
+        i += 1
