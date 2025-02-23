ef07405edb77e95d14697ee6a650b34b9182eb6b
Hayyaun <hayyaun@outlook.com>
2025-02-23 18:48:39 +0330

    update

--- SCORES.md
+++ SCORES.md
@@ -4,6 +4,7 @@ Here is a list of top works of our students.
 
 | Work                                        | Student     | Description                                                                                |
 | ------------------------------------------- | ----------- | ------------------------------------------------------------------------------------------ |
+| 🧮 [GUI Calculator](/works/barbod_calc.py)  | Barbod Dal. | GUI Calculator with cool features.                                                         |
 | 🃏 [OIIAI](/works/barbod_oiiai.py)          | Barbod Dal. | Useless and meaningless program with no fun! don't waste your time running it              |
 | 🎰 [Chance](/works/ali_gamble.py)           | Ali Saf.    | Amazing and fun game to try your luck, also don't forget to cheat!                         |
 | 🔮 [Sphere](/works/noyan_sphere.py)         | Noyan Gol.  | Calculates surface area and volume of spheres, circles, and cylinders.                     |
--- works/barbod_calc.py
+++ works/barbod_calc.py
@@ -4,15 +4,22 @@ from tkinter import *
 equation = ""
 
 
-def caculator(s: str):
-    def calculate():
+def append_equation(s):
+    def append_s():
         global equation
         equation += s
         l_equation.config(text=equation)
-    return calculate
+    return append_s
 
 
-def evaluate():
+def delete():
+    global equation
+    equation = ""
+    l_equation.config(text=equation)
+    l_answer.config(text=equation)
+
+
+def calculate():
     answer = eval(equation)
     l_answer.config(text=answer)
 
@@ -25,49 +32,63 @@ master.geometry('450x400')
 master.configure(bg='gray')
 
 # labels
-l_equation = Label(master, font="20", bg="white", width=25, anchor="e")
+l_equation = Label(master, font="20", bg="white", width=24, anchor="e")
 l_equation.place(x=120, y=30)
 l_equal = Label(master, text='=', font='20', bg='gray',)
 l_equal.place(x=120, y=70)
-l_answer = Label(master, font='20', width=7)
-l_answer.place(x=300, y=70)
+l_answer = Label(master, font='20', width=12)
+l_answer.place(x=240, y=70)
 
 # numbers
 n1 = Button(master, text='1', font='16', width=4,
-            height=2, command=caculator('1'))
+            height=2, command=append_equation('1'))
 n1.place(x=120, y=110)
-n2 = Button(master, text='2', font='16', width=4, height=2)
+n2 = Button(master, text='2', font='16', width=4,
+            height=2, command=append_equation('2'))
 n2.place(x=180, y=110)
-n3 = Button(master, text='3', font='16', width=4, height=2)
+n3 = Button(master, text='3', font='16', width=4,
+            height=2, command=append_equation('3'))
 n3.place(x=240, y=110)
-n4 = Button(master, text='4', font='16', width=4, height=2)
+n4 = Button(master, text='4', font='16', width=4,
+            height=2, command=append_equation('4'))
 n4.place(x=120, y=170)
-n5 = Button(master, text='5', font='16', width=4, height=2)
+n5 = Button(master, text='5', font='16', width=4,
+            height=2, command=append_equation('5'))
 n5.place(x=180, y=170)
-n6 = Button(master, text='6', font='16', width=4, height=2)
+n6 = Button(master, text='6', font='16', width=4,
+            height=2, command=append_equation('6'))
 n6.place(x=240, y=170)
-n7 = Button(master, text='7', font='16', width=4, height=2)
+n7 = Button(master, text='7', font='16', width=4,
+            height=2, command=append_equation('7'))
 n7.place(x=120, y=230)
-n8 = Button(master, text='8', font='16', width=4, height=2)
+n8 = Button(master, text='8', font='16', width=4,
+            height=2, command=append_equation('8'))
 n8.place(x=180, y=230)
-n9 = Button(master, text='9', font='16', width=4, height=2)
+n9 = Button(master, text='9', font='16', width=4,
+            height=2, command=append_equation('9'))
 n9.place(x=240, y=230)
-n10 = Button(master, text='AC', font='16', bg='red', width=4, height=2)
+n10 = Button(master, text='AC', font='16', bg='red', width=4,
+             height=2, command=delete)
 n10.place(x=120, y=290)
-n0 = Button(master, text='0', font='16', width=4, height=2)
+n0 = Button(master, text='0', font='16', width=4,
+            height=2, command=append_equation('0'))
 n0.place(x=180, y=290)
 
 # signs
 eq = Button(master, text='=', font='16', bg='purple',
-            width=4, height=2, command=evaluate)
+            width=4, height=2, command=calculate)
 eq.place(x=240, y=290)
-plus = Button(master, text='+', font='16', bg='purple', width=4, height=2)
+plus = Button(master, text='+', font='16', bg='purple',
+              width=4, height=2, command=append_equation('+'))
 plus.place(x=300, y=290)
-minus = Button(master, text='-', font='16', bg='purple', width=4, height=2)
+minus = Button(master, text='-', font='16', bg='purple',
+               width=4, height=2, command=append_equation('-'))
 minus.place(x=300, y=230)
-prod = Button(master, text='*', bg='purple', font='16', width=4, height=2)
+prod = Button(master, text='*', bg='purple', font='16',
+              width=4, height=2, command=append_equation('*'))
 prod.place(x=300, y=170)
-div = Button(master, text='÷', font='16', bg='purple', width=4, height=2)
+div = Button(master, text='÷', font='16', bg='purple',
+             width=4, height=2, command=append_equation('/'))
 div.place(x=300, y=110)
 
 # run
