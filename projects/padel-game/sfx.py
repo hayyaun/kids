e55be214afe585cf65f59cd7eb4388be71888e6e
Hayyaun <hayyaun@outlook.com>
2025-03-30 14:43:05 +0330

    update

--- projects/padel-game/sfx.py
+++ projects/padel-game/sfx.py
@@ -1,3 +1,12 @@
+def play_sound(file: str):
+    try:
+        from playsound import playsound
+        print('Playing sound: ', file)
+        playsound(file)
+    except:
+        print('Nemitunam seda play konam!')
+
+
 def timer_sfx():
     return
 
