9ddf554fcea2aa54beb86841e9d6f6dd65006fb9
Hayyaun <hayyaun@outlook.com>
2025-03-30 16:21:15 +0330

    update

--- projects/padel-game/rockets.py
+++ projects/padel-game/rockets.py
@@ -21,12 +21,12 @@ def check_ball_hit_rocket(ball: turtle.Turtle, left_rocket: turtle.Turtle, right
 
     # 1. بررسی برخورد توپ با راکت سمت چپ
     # اگه توپ هم از لحاظ افقی و هم عمودی نزدیک راکت بود: توپ برمیگرده
-    if abs(ball_x - left_rocket_x) < hit_threshold_x and (left_rocket_y - hit_threshold_y < ball_y < left_rocket_y + hit_threshold_y):
+    if abs(ball_x - left_rocket_x) < hit_threshold_x and abs(ball_y - left_rocket_y) < hit_threshold_y:
         ball.dx *= -1  # تغییر جهت حرکت افقی توپ
         # جابجایی توپ به بیرون از راکت برای جلوگیری از عبور
         ball.setx(left_rocket_x + hit_threshold_x)
 
     # 2. بررسی برخورد توپ با راکت سمت راست
-    if abs(ball_x - right_rocket_x) < hit_threshold_x and (right_rocket_y - hit_threshold_y < ball_y < right_rocket_y + hit_threshold_y):
+    if abs(ball_x - right_rocket_x) < hit_threshold_x and abs(ball_y - right_rocket_y) < hit_threshold_y:
         ball.dx *= -1
         ball.setx(right_rocket_x - hit_threshold_x)
