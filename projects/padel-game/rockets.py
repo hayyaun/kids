72f5301fc9e299224596b3ab085aeea91fe1802a
Hayyaun <hayyaun@outlook.com>
2025-03-30 16:18:30 +0330

    update rockets

--- projects/padel-game/rockets.py
+++ projects/padel-game/rockets.py
@@ -15,15 +15,18 @@ def check_ball_hit_rocket(ball: turtle.Turtle, left_rocket: turtle.Turtle, right
     ''' محاسبات اصلی '''
 
     # 1. بررسی برخورد توپ با راکت سمت چپ
+
+    hit_threshold_x = 10  # فاصله افقی مجاز برای برخورد
+    hit_threshold_y = 50  # فاصله عمودی راکت که توپ باید داخلش باشه
+
+    # 1. بررسی برخورد توپ با راکت سمت چپ
     # اگه توپ هم از لحاظ افقی و هم عمودی نزدیک راکت بود: توپ برمیگرده
-    if (left_rocket_x - 5 < ball_x < left_rocket_x + 5) and (left_rocket_y - 50 < ball_y < left_rocket_y + 50):
-        # منظور از برگشتن توپ یعنی جهت حرکت افقیش برعکس میشه
-        ball.dx *= -1
-        # همچنین باید به محل قبلی برای شروع مجدد برگرده یعنی
-        ball.setx(left_rocket_x)
+    if abs(ball_x - left_rocket_x) < hit_threshold_x and (left_rocket_y - hit_threshold_y < ball_y < left_rocket_y + hit_threshold_y):
+        ball.dx *= -1  # تغییر جهت حرکت افقی توپ
+        # جابجایی توپ به بیرون از راکت برای جلوگیری از عبور
+        ball.setx(left_rocket_x + hit_threshold_x)
 
     # 2. بررسی برخورد توپ با راکت سمت راست
-    # مراحل قبل این بار برای راکت سمت راست تکرار میشه...
-    if (right_rocket_x - 5 < ball_x < right_rocket_x + 5) and (right_rocket_y - 50 < ball_y < right_rocket_y + 50):
+    if abs(ball_x - right_rocket_x) < hit_threshold_x and (right_rocket_y - hit_threshold_y < ball_y < right_rocket_y + hit_threshold_y):
         ball.dx *= -1
-        ball.setx(right_rocket_x)
+        ball.setx(right_rocket_x - hit_threshold_x)
