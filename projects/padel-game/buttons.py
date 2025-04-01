c93d587111385e8a5d5960b90d74475f53c0293d
Hayyaun <hayyaun@outlook.com>
2025-04-01 14:30:37 +0330

    btns

---
+++ projects/padel-game/buttons.py
@@ -0,0 +1,19 @@
+import turtle
+
+
+def show_pause_btn():
+    # فقط یه دکنه استوپ سمت راست تابلو امتیازات اضافه شه
+    # TODO
+    return
+
+
+def show_reset_btn(reset_btn: turtle.Turtle, show: bool):
+    # clean previous reset_btn
+    # دکمه ریست قبلی پاک بشه
+    # TODO
+    # create new_reset_btn
+    # دکمه جدید رسم بشه
+    # TODO
+    # return new_reset_btn
+    # دکمه جدید برگردونده شه
+    return  # TODO
